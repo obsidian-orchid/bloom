05f29acd63dcd457ea2a19e60735018d7582dc35
hridhya <menonhridhya@gmail.com>
2016-02-03 15:55:34 -0800

    services enabled are back

--- client/templates/home.jsx
+++ client/templates/home.jsx
@@ -44,30 +44,14 @@ Home = React.createClass({
 	},
   renderServices(){
     if(this.data.currentUser !== undefined) {
-      var services = Object.keys(this.data.currentUser.services);
-      services.splice(0,3);
+      var services = [];
       for(var key in this.data.currentUser.services) {
-        if (key === 'google' && !key.hasOwnProperty('access_token')) {
-          var index = services.indexOf(key);
-          services.splice(index, 1);
-        }
-        if (key === 'imgur' && !key.hasOwnProperty('token')) {
-          var index = services.indexOf(key);
-          services.splice(index, 1);
-        }
-        if (key === 'facebook' && !key.hasOwnProperty('accesstoken')) {
-          var index = services.indexOf(key);
-          services.splice(index, 1);
-        }
-        if (key === 'twitter' && !key.hasOwnProperty('token')) {
-          var index = services.indexOf(key);
-          services.splice(index, 1);
-        }
-        if (key === 'pinterest' && !key.hasOwnProperty('token')) {
-          var index = services.indexOf(key);
-          services.splice(index, 1);
+        console.log(key, this.data.currentUser.services[key].hasOwnProperty('accessToken'));
+        if(this.data.currentUser.services[key].hasOwnProperty('accessToken')){
+          services.push(key);
         }
       }
+      console.log(services);
       return services.map((service) => {
         return <EnabledServices key={service} service={service} />;
       });
@@ -126,7 +110,6 @@ Home = React.createClass({
       <div className="">
         <div className="row">
           <div className="col s12">
-            <p className="flow-text">ENABLED SERVICES</p>
             <ul className="tabs">
               {this.renderServices()}
             </ul>
@@ -168,7 +151,7 @@ Home = React.createClass({
 EnabledServices = React.createClass({
   render(){
     return (
-      <a href="" onClick={this.selected} className="tab col s3"><li className={this.state.condition ? "selected": ""} >{this.props.service}</li></a>
+      <li className="tab col s3">{this.props.service}</li>
     )
   }
 });
