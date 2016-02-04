bfa46ec7e4549df6b3c59bae3dee020531b158b0
justshiv <accounts@justshiv.com>
2016-02-04 11:03:04 -0800

    added comments

--- client/templates/mainlayout.jsx
+++ client/templates/mainlayout.jsx
@@ -1,15 +1,19 @@
-
+//Our main template inside of which we display all other templates
 MainLayout = React.createClass({
 
+  //Required to import react data
   mixins: [ReactMeteorData],
 
+  //Retrieving reactive data
   getMeteorData(){
     return {
-      //returning alphabetically sorted services
       currentUser: Meteor.user()
     };
   },
 
+  //Adding elements to template on page load. Here we add the `collapseOnClick`
+  //attribute of the [Materialize](http://materializecss.com/) `sideNav` element
+  //and append to the generated header
   componentDidMount() {
     $(".button-collapse").sideNav({
       closeOnClick: true //closes when we click things
@@ -18,14 +22,11 @@ MainLayout = React.createClass({
     $('head').append('<link rel="shortcut icon" type="image/png" href="favicon.png">');
     $('head').append('<link rel="shortcut icon" type="image/png" href="favicon.png">');
   },
+
+  //render method to create whole page
+  //note the reactive logic to determine whether to show the "Sign In" or "Sign Out"
+  //buttons
   render() {
-    var authBtn;
-    if(!Meteor.user()){
-      authBtn = <li><a href="/signin">Sign In</a></li>
-    }
-    else{
-      authBtn = <li><a onClick={this.signOut}>Sign Out</a></li>
-    }
     return (
       <div className="page-content">
         <title>SNAPSHARE</title>
@@ -38,11 +39,11 @@ MainLayout = React.createClass({
             <a href="" data-activates="mobile-demo" className="button-collapse"><i className="mdi-navigation-menu"></i></a>
             <ul className="right hide-on-med-and-down">
               <li><a href="/services">Services</a></li>
-              {authBtn}
+              <li><a href={this.data.currentUser ? "" : "/signin"} onClick={this.data.currentUser ? this.signOut : ""}>{this.data.currentUser ? "Sign Out" : "Sign In" }</a></li>
             </ul>
             <ul className="side-nav" id="mobile-demo">
               <li><a href="/services">Services</a></li>
-              {authBtn}
+              <li><a href={this.data.currentUser ? "" : "/signin"} onClick={this.data.currentUser ? this.signOut : ""}>{this.data.currentUser ? "Sign Out" : "Sign In" }</a></li>
             </ul>
           </div>
         </nav>
@@ -68,9 +69,9 @@ MainLayout = React.createClass({
         </footer>
       </div>
     )
-
   },
 
+  //method to sign out users and redirect them to sign in page with nice popup
   signOut(event){
     event.preventDefault();
     Meteor.logout();
