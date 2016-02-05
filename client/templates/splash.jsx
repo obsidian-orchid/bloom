8b85b67b40734a6b0005479a7f99334b3e4a5dc1
justshiv <accounts@justshiv.com>
2016-02-04 17:18:31 -0800

    refactored adding & posting UI to make it more streamlined

---
+++ client/templates/splash.jsx
@@ -0,0 +1,11 @@
+Splash = React.createClass({
+  render() {
+    return (
+      <div className="row">
+        <div className="col l6 offset-l3 m8 offset-m2 s12" onSubmit={this.signIn}>
+              wowowowow splash
+        </div>
+      </div>
+    )
+  }
+});
