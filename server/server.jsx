4a85f60c78d3d2b766532b00b8b86703141dac81
justshiv <accounts@justshiv.com>
2016-02-06 11:39:58 -0800

    added basic info for pinterest

--- server/server.jsx
+++ server/server.jsx
@@ -102,3 +102,16 @@ Accounts.onCreateUser(function(options, user){
 Accounts.config({
   sendVerificationEmail: true
 });
+
+//Meteor.startup(function() {
+//    SSLProxy({
+//       port: 3001, //or 443 (normal port/requires sudo)
+//       ssl : {
+//            key: Assets.getText("server.key"),
+//            cert: Assets.getText("server.crt"),
+//
+//            //Optional CA
+//            //Assets.getText("ca.pem")
+//       }
+//    });
+//});
